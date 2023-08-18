e259a8e3be4b25f4d85c1347555abd8b9d881a48
CatzillaOrz <catzillaorz@outlook.com>
2023-08-18 09:52:50 +0800

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,34 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Avatar />
+      <Intro />
+      <SkillList />
     </div>
   );
+}
+
+function Avatar() {
+  return <img className="" src="" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jack Mellon</h1>
+      <p> This is bref of Jack's CV</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div>
+      <Skill />
+    </div>
+  );
+}
+
+function Skill() {
+  return <div className="">Use React</div>;
 }
